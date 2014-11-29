4ce3807f4195fb02276e7ee0a68e25a54d74f383
digital <digital@dev.monitorix.com.ua>
2014-11-29 16:06:02 +0100

    Recognition test added

---
+++ core/testrecognition.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+from recognizer import text_processor
+
+from BeautifulSoup import BeautifulSoup
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
+with open(filename1, 'r') as f:
+    filetext1 = f.readlines()
+with open(filename2, 'r') as f:
+    filetext2 = f.readlines()
+
+idealmodel = ''
+debug = 0
+(htmltest, structure, fulltext) = text_processor(filetext1, idealmodel, debug)
+(htmltest, structure, fulltext) = text_processor(filetext2, htmltest, '')
+for line in fulltext:
+    if line:
+        print line
