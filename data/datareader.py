2a6d3a0f5161e21cc457fe98ccfe964ce04ae41d
digital <digital@dev.monitorix.com.ua>
2014-12-07 22:50:04 +0100

    Locations list edited

--- data/datareader.py
+++ data/datareader.py
@@ -4,10 +4,18 @@ import sys
 import json
 import simplejson
 filename = "nlgeo.json.1"
+filename = "nlgeo.json"
 
 with open(filename) as json_file:
     json_data = json.load(json_file)
     loc = json_data['locations']
 
+    cities = {}
     for cityline in loc:
-	print cityline
+	year = cityline['year']
+	cityname = cityline['naam']
+	if cities.has_key(cityname):
+	    cities[cityname] = int(cities[cityname]) + 1 
+	else:
+	    print str(year) + ',' + cityline['amsterdam_code'] + ',' + str(cityline['naam'])
+	    cities[cityname] = 1
