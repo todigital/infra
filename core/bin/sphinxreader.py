c297d9645e8b442abe34dad35ed0d1a08797dad3
digital <digital@dev.monitorix.com.ua>
2014-12-09 00:50:31 +0100

    Bug with missing XML fixed

--- core/bin/sphinxreader.py
+++ core/bin/sphinxreader.py
@@ -7,10 +7,10 @@ s = Server('http://127.0.0.1:5984/')
 
 # default database
 database = 'nltest'
-day = sys.argv[1]
-if day:
+userdb = sys.argv[1]
+if userdb:
     #nl_20141203
-    database = "nl_" + day
+    database = userdb
 db = s[database]
 
 print "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
@@ -30,38 +30,41 @@ id = 0
 # Create XML
 for docid in db:
     item = db.get(docid)
-    id = id + 1
-    print "	<sphinx:document id=\"" + str(id) + "\">"
-    for field in item:
-	showline = 1
-	if field == '_id1':
-	    showline = 0
-	if field == 'json1':
-	    showline = 0
-	if field == '_rev':
-            showline = 0
+    try:
+        id = id + 1
+        print "	<sphinx:document id=\"" + str(id) + "\">"
+        for field in item:
+	    showline = 1
+	    if field == '_id1':
+	        showline = 0
+	    if field == 'json1':
+	        showline = 0
+	    if field == '_rev':
+                showline = 0
 
-	if showline:
-	    thisfield = field
-	    replaced = item[field]
-	    removehtml = ''
-	    if thisfield == 'json':
-		removehtml = 1
-	    if thisfield == 'content':
-		removehtml = 1
+	    if showline:
+	        thisfield = field
+	        replaced = item[field]
+	        removehtml = ''
+	        if thisfield == 'json':
+		    removehtml = 1
+	        if thisfield == 'content':
+		    removehtml = 1
 
-	    if removehtml:
-		s = item[field]
-	        replaced = re.sub('<', '&lt;', s)
-		replaced = re.sub('>', '&gt;', replaced)
-		replaced = re.sub('&', '&amp;', replaced)
-		if replaced:
-	 	    item[field] = replaced
-	    if thisfield == 'url':
-		s = item[field]
-	        replaced = re.sub('&', '&amp;', replaced)
-		if replaced:
-                    item[field] = replaced
-            print "	<" + field + '>' + item[field] + "</" + field + ">"
-    print "	</sphinx:document>"
+	        if removehtml:
+		    s = item[field]
+	            replaced = re.sub('<', '&lt;', s)
+		    replaced = re.sub('>', '&gt;', replaced)
+		    replaced = re.sub('&', '&amp;', replaced)
+		    if replaced:
+	 	        item[field] = replaced
+	        if thisfield == 'url':
+		    s = item[field]
+	            replaced = re.sub('&', '&amp;', replaced)
+		    if replaced:
+                        item[field] = replaced
+                print "	<" + field + '>' + item[field] + "</" + field + ">"
+        print "	</sphinx:document>"
+    except:
+	skip = 1
 print "</sphinx:docset>"
