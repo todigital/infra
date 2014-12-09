1b9c6da4384f6418e5542b40b018f5a8eb5f6449
digital <digital@dev.monitorix.com.ua>
2014-12-10 00:34:27 +0100

    Add import script to dataware

---
+++ import/import2pg.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use vars qw/$libpath/;
+use FindBin qw($Bin);
+BEGIN { $libpath="$Bin" };
+use lib "$libpath";
+use lib "$libpath/../libs";
+
+use DB_File;
+use DBI;
+$| = 1;
+
+use Getopt::Std;
+%options=();
+getopts("od:f:",\%options);
+# like the shell getopt, "d:" means d takes an argument
+$file = $options{o} if defined $options{o};
+$dir = $options{d} if defined $options{d};
+
+my %dbconfig = loadconfig("/etc/apache2/infra.conf");
+$site = $dbconfig{root};
+my ($dbname, $dbhost, $dblogin, $dbpassword) = ($dbconfig{dbname}, $dbconfig{dbhost}, $dbconfig{dblogin}, $dbconfig{dbpassword});
+my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$dbhost",$dblogin,$dbpassword,{AutoCommit=>1,RaiseError=>1,PrintError=>0});
+
+push(@files, $file) if ($file);
+
+foreach $file (@files)
+{
+    print "$file\n";
+}
+
+sub loadconfig
+{
+    my ($configfile, $DEBUG) = @_;
+    my %config;
+
+    open(conf, $configfile);
+    while (<conf>)
+    {
+        my $str = $_;
+        $str=~s/\r|\n//g;
+        my ($name, $value) = split(/\s*\=\s*/, $str);
+        $config{$name} = $value;
+    }
+    close(conf);
+
+    return %config;
+}
