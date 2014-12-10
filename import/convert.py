ce262f9f73c19482c1b8b94766ceef8d3300bd04
digital <digital@dev.monitorix.com.ua>
2014-12-10 20:57:28 +0100

    utf-8 convertor

--- import/convert.py
+++ import/convert.py
@@ -3,11 +3,12 @@
 import os
 import sys
 import shutil
-
+import chardet
+ 
 def convert_to_utf8(filename):
     # gather the encodings you think that the file may be
     # encoded inside a tuple
-    encodings = ('koi8-r', 'windows-1251', 'windows-1253', 'iso-8859-7', 'macgreek')
+    encodings = ('windows-1251', 'koi8-r', 'windows-1253', 'iso-8859-7', 'macgreek')
  
     # try to open the file and exit if some IOError occurs
     try:
@@ -15,33 +16,17 @@ def convert_to_utf8(filename):
     except Exception:
         sys.exit(1)
  
-    # now start iterating in our encodings tuple and try to
-    # decode the file
-    for enc in encodings:
-        try:
-            # try to decode the file with the first encoding
-            # from the tuple.
-            # if it succeeds then it will reach break, so we
-            # will be out of the loop (something we want on
-            # success).
-            # the data variable will hold our decoded text
-            data = f.decode(enc)
-            break
-        except Exception:
-            # if the first encoding fail, then with the continue
-            # keyword will start again with the second encoding
-            # from the tuple an so on.... until it succeeds.
-            # if for some reason it reaches the last encoding of
-            # our tuple without success, then exit the program.
-            if enc == encodings[-1]:
-                sys.exit(1)
-            continue
- 
+    result = chardet.detect(f)
+    charset = result['encoding']
+    if charset == 'utf-8':
+        return
+    else:
+        data = f.decode(charset)
+
     # now get the absolute path of our filename and append .bak
     # to the end of it (for our backup file)
     fpath = os.path.abspath(filename)
-    newfilename = fpath + '.bak'
-    newfilename = 'test.bak'
+    newfilename = fpath + '.orig'
     # and make our backup file with shutil
     shutil.copy(filename, newfilename)
  
