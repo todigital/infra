5cf2dc2b0885f4c6cd7712995b2342312d1f54f7
digital <digital@dev.monitorix.com.ua>
2014-12-10 21:56:40 +0100

    Import to dataware added

--- import/import2pg.pl
+++ import/import2pg.pl
@@ -48,9 +48,18 @@ foreach $dir (@dirs)
     @dir = readdir(DIR);
     foreach $file (@dir)
     {
-	if ($file=~/\w+/ && $file!~/\.start/)
+	if ($file=~/\w+/ && $file!~/(\.start|\.bak|\.orig)/)
 	{
-	    push(@files, "$dir/$file");
+	    $path = "$dir/$file";
+	    $convert = `$Bin/convert.py $dir/$file`;
+	    push(@files, "$path");
+	}
+	if ($file=~/\.bak/)
+	{
+	    $ofile = $file;
+	    $ofile=~s/\.bak//g;
+	    $mv = `/bin/mv $dir/$file $dir/$ofile`;
+	    print "$dir/$file\n";
 	}
     }
     closedir(DIR);
@@ -58,7 +67,68 @@ foreach $dir (@dirs)
 
 foreach $file (@files)
 {
-    print "$file\n";
+    if ($file=~/\/(\d{4})(\d{2})(\d{2})\/(\d+)/)
+    {
+        my $date = sprintf("%04d-%02d-%02d %02d:00", $1, $2, $3, $4);
+        $dates{$file} = $date;
+    }
+
+    print "$file\n" if ($DEBUG);
+    my ($url, $root, $title, $html, $content, $text) = readhtml($file);
+    $title = $dbh->quote($title);
+    $html = $dbh->quote($html);  
+    $content = $dbh->quote($content);
+    $text = $dbh->quote($text);
+    $id = '0';
+    if ($url && !$known{$url})
+    {
+        $sql = "insert into news (title, html, content, text, url, root, founddate) values ($title, $html, $content, $text, '$url', '$root', '$dates{$file}');";
+	$dbh->do($sql);
+	#print "$sql\n";
+    }
+    $known{$url}++;
+}
+
+sub readhtml
+{
+    my ($filename, $DEBUG) = @_;
+    my ($title, $root, $html, $text);
+
+    $convert = `$Bin/convert.py $filename`;
+    open my $fh, "<:encoding(utf8)", $filename or die "$filename\n";
+    binmode STDOUT, ':utf8';
+    @content = <$fh>;
+    close $fh;
+
+    $url = shift @content;
+    if ($url=~/Monitorix\-url\:\s+(.+)/)
+    {
+	$url=$1;
+	if ($url=~/^(http\:\/\/\S+?)\//)
+	{
+	   $root = $1;
+	}
+    }
+
+    $html = "@content";
+    if ($html=~/<title>(.+?)(<\/title>)/sxi)
+    {
+	$title = $1;
+    }
+    $title = "notitle" unless ($title);
+    $text = $title;
+    $content = $html; 
+    $content=~s/<(?:[^>'"]*|(['"]).*?\1)*>//gsx;
+    if ($DEBUG)
+    {
+    #print  "@content\n";
+    print "$title\n";
+    print "$content\n";
+    };
+
+    #print "$url $root\n";
+    print "$filename\n";
+    return ($url, $root, $title, $html, $content, $text);
 }
 
 sub loadconfig
