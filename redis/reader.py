51dd1129f69f07786acc25c370829b3d339977c8
tikhonov <illugion4digital.com>
2015-01-18 14:05:47 +0100

    Codes added

--- redis/reader.py
+++ redis/reader.py
@@ -1,12 +1,52 @@
 #!/usr/bin/python
-
 import redis
 import shutil
 import chardet
+from recognizer import text_processor, text_processor_test
+from BeautifulSoup import BeautifulSoup
 
 redis = redis.Redis(host='localhost', port=6379, db=0)
-limit = 30
+limit = 2
 count = 0
+idealmodel = ''
+debug = 0
+
+def buildpattern(html, debug):
+    result = {}
+    structure = []
+    fulltext = []
+    title = ''
+    attributes = {}
+    
+    htmlstrings = html.splitlines()
+    if htmlstrings:
+        lineID = 0
+        for line in htmlstrings:
+            lenstr = len(line)
+            words = len(line.split())
+            comas = len(line.split(","))
+            dots = len(line.split("."))
+            equal = len(line.split("="))
+            soup = BeautifulSoup(line)
+            if words:
+                htmltags = []
+                for child in soup.recursiveChildGenerator():
+                    name = getattr(child, "name", None)
+                    if name is not None:
+                         htmltags.append(name)
+                    elif not child.isspace(): # leaf node, don't print spaces
+                         donothing = 1
+                code = 'W' + str(words) + ',C' + str(comas) + ',D' + str(dots) + ',E' + str(equal)
+                result[lineID] = code
+		lineID = lineID + 1
+
+    if debug:
+	sorted(result, key=int)
+	for lineID in result:
+	    code = result[lineID]
+            print str(lineID) + ',' + code
+    
+    return result
 
 keys = redis.keys('*');
 for key in keys:
@@ -25,5 +65,6 @@ for key in keys:
         else:
             data = html.decode(charset)
 
-        print data
+        pattern = buildpattern(data, '1')
+        #(htmltest, structure, fulltext, attr) = text_processor(data, idealmodel, debug)
     count = count + 1
