799e818ea03f977ba16318a4727103ec8761c09b
tikhonov <illugion4digital.com>
2015-01-18 12:20:40 +0100

    Get urls as test

--- redis/download.py
+++ redis/download.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+
 import urllib
 import re
 import sys
 import redis
 redis = redis.Redis(host='localhost', port=6379, db=0)
 
+root = "http://www.pravda.com.ua"
 key = "http://www.pravda.com.ua/news"
 url = "http://www.pravda.com.ua/news/2015/01/16/7055345/"
 
@@ -13,7 +15,22 @@ def download(url):
     html = page.read()
     return html
 
+def geturls(page):
+    links = re.findall(r'href=[\'"]?([^\'" >]+)', page)
+    urls = {}
+    for url in links:
+        regexp = r'news\\S+'
+        match = re.match(r'\/news\S+', url)
+        if match:
+	    fullurl = root + url
+            urls[fullurl] = url
+    return urls
+
 page = download(url)
+urls = geturls(page)
 redis.set(key, url)
 redis.set(url, page)
-print page
+for url in urls:
+    html = download(url)
+    #urls = geturls(page)
+    redis.set(url, html)
