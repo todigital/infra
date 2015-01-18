b8efa2970d0ce8db47e1f397f5cebd4769eff00e
tikhonov <illugion4digital.com>
2015-01-18 12:47:09 +0100

    Test added

--- redis/reader-test.py
+++ redis/reader-test.py
@@ -6,14 +6,21 @@ redis = redis.Redis(host='localhost', port=6379, db=0)
 keys = redis.keys('*');
 for key in keys:
     type = redis.type(key);
-    print key
+    value = ''
+    if type == 'string':
+        val = redis.get(key);
+        value = val
     if type == 'KV':
         val = redis.get(key);
+	value = val
     if type == 'HASH':
         vals = redis.hgetall(key);
+	value = vals
     if type == 'ZSET':
         vals = redis.zrange(key, 0, -1);
+	value = vals
     print key
+    print value
 
 #file = argv[1]
 
