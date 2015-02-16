de8397b79a90936e61ee4effed21bc68b718ca56
¨Vyacheslav <4tikhonov@gmail.com>
2015-02-16 17:33:00 +0100

    Clusters added

---
+++ production/clusters.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+def makeclusters(maxDistance, doc, mainindex):
+    previd = 0
+    clusters = {}
+    rowcluster = []
+    clusterID = 0
+    maxDistance = maxDistance / 2
+    #maxDistance = 10
+    clusterRank = {}
+
+    try:
+        for id in mainindex:
+            x = id
+            item = doc[id]
+            words = item['visiblewords']
+            comas = item['comas']
+            dots = item['dots']
+            code = item['code']
+            try:
+                Distance = rank[id]
+            except:
+                Distance = maxDistance
+
+            if words <= 5:
+                Distance = -1
+                if comas <= 1:
+                    if dots <= 1:
+                        Distance = -1
+
+            if item['date']:
+                if item['timeflag']:
+                    Distance = -1
+
+            delta = id - previd
+            print '[' + str(id) + '] ' + str(delta) + ' ' + code + ' ' + str(Distance) + ' ' + str(words)
+            if abs(delta) <= Distance:
+                # Extend cluster
+                try:
+                    rowcluster = clusters[clusterID]
+                except:
+                    rowcluster = []
+                    clusterRank[clusterID] = 0
+                rowcluster.append(id)
+                clusters[clusterID] = rowcluster
+            else:
+                # New cluster
+                clusterID = clusterID + 1
+                rowcluster = []
+                rowcluster.append(id)
+                clusters[clusterID] = rowcluster
+                clusterRank[clusterID] = 0
+
+            clusterRank[clusterID] = clusterRank[clusterID] + int(words)
+            previd = id
+    except:
+	print "Warning: error in cluster...\n";
+
+    return clusterRank
